72bcb0178eef849e7b4cbd21a0f5129addc9e9f6
sristisravan <sristisravan@gmail.com>
2022-01-24 21:52:16 +0800

    Added files

---
+++ environment/tools-install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+VAGRANT_HOST_DIR=/mnt/host_machine
+
+########################
+# Jenkins & Java
+########################
+echo "Installing Jenkins and Java"
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key  | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get -y install default-jdk jenkins > /dev/null 2>&1
+echo "Installing Jenkins default user and config"
+sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/config.xml /var/lib/jenkins/
+sudo mkdir -p /var/lib/jenkins/users/admin
+sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/users/admin/config.xml /var/lib/jenkins/users/admin/
+sudo chown -R jenkins:jenkins /var/lib/jenkins/users/
+
+########################
+# Node & npm
+########################
+echo "Installing Node & npm"
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
+
+########################
+# Docker
+########################
+echo "Installing Docker"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+sudo systemctl enable docker
+sudo usermod -aG docker ${USER}
+sudo usermod -aG docker jenkins
+
+########################
+# nginx
+########################
+echo "Installing nginx"
+sudo apt-get -y install nginx > /dev/null 2>&1
+sudo apt-get install -y python3-pip
+sudo service nginx start
+
+########################
+# Configuring nginx
+########################
+echo "Configuring nginx"
+cd /etc/nginx/sites-available
+sudo rm default ../sites-enabled/default
+sudo cp /mnt/host_machine/VirtualHost/jenkins /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
+sudo service nginx restart
+sudo service jenkins restart
+echo "Success"
+
+#docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+#git 
+sudo apt-get install -y git
+
+# MySQL install
+# Download and Install the Latest Updates for the OS
+sudo apt-get upgrade -y
+
+# Set the Server Timezone to CST
+echo "America/Chicago" > /etc/timezone
+sudo dpkg-reconfigure -f noninteractive tzdata
+
+# Enable Ubuntu Firewall and allow SSH & MySQL Ports
+sudo ufw enable
+sudo ufw allow 22
+sudo ufw allow 3306
+
+# Install essential packages
+sudo apt-get -y install zsh htop
+
+# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
+echo "mysql-server-5.6 mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password root" | sudo debconf-set-selections
+sudo apt-get -y install mariadb-server-10.5
