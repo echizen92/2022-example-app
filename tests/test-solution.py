9946ee287e9d6978c38b72aa1cc0ca344e824165
Faiz Rashid <faiz.rashid@gmx.us>
2022-02-02 22:19:17 +0800

    pytest test-solution.py submission

--- tests/test-solution.py
+++ tests/test-solution.py
@@ -1,28 +1,34 @@
-import requests
-import pytest
+import requests 
+from bs4 import BeautifulSoup
 
 # login test cases
     
 def test_login_1():
     session = requests.session()
     login = session.post("http://localhost:9988/login", {"username": "test@user.com", "password": "password"}).text
-    print ("Successful login")
-    #assert fault_text == "Successful login"
+    soup = BeautifulSoup(login, "html.parser")
+    print(soup.title.string)
+    fault_text = soup.title.string
+    assert fault_text == "ChaimTube"
 
 def test_login_2():
     session = requests.session()
     login = session.post("http://localhost:9988/login", {"username": "test@user.com", "password": "pass"}).text
-    fault_text = login 
-    assert fault_text == "Wrong password"
+    soup = BeautifulSoup(login, "html.parser")
+    print(soup.title.string)
+    fault_text = soup.title.string
+    assert fault_text == "Invalid login!" 
+
     
 def test_login_3():
     session = requests.session()
     login = session.post("http://localhost:9988/login", {"username": "root@user.com", "password": "password"}).text
-    fault_text = login
-    assert fault_text == "Wrong username"
+    soup = BeautifulSoup(login, "html.parser")
+    print(soup.title.string)
+    fault_text = soup.title.string
+    assert fault_text == "Invalid login!"
 
 if __name__ == '__main__':
     test_login_1()
     test_login_2()
     test_login_3()
-
